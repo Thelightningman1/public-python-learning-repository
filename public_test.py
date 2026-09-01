8e14c80946979b896fc0d37a9f376cafad1fd152
Thelightningman1 <noahsecher8@gmail.com>
2026-09-01 18:04:52 +0200

    Day 1: Public Python basics

---
+++ public_test.py
@@ -0,0 +1,8 @@
+print ("hello world") 
+print ("i am learning python")
+print (10 + 20) 
+print ("my name is thelightningman1")
+name = "thelightningman1"
+print (name) 
+input ("what is your name?")
+print ("my name is" + name)
